27ed3fc6ff75231a53811bb3945d465d7b3f1c2e
Cara Lisy <lisyce@gmail.com>
2022-09-10 10:40:36 -0700

    can render all a user's threads

--- django_app/tracker/urls.py
+++ django_app/tracker/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', views.home),
+    path('collection/', views.collection)
+    # rendering react stuff
+    # path('', TemplateView.as_view(template_name='index.html'))
 ]
--- django_app/tracker/views.py
+++ django_app/tracker/views.py
@@ -1,6 +1,11 @@
-import re
+from .models import *
+from django.contrib.auth import models as auth_models
+
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'tracker/index.html')
+
+def collection(request):
+    user_threads = UserThread.objects.filter(owner=auth_models.User.objects.get(username='admin'))
+    return render(request, 'tracker/collection.html', {'user_threads':user_threads})
