d5737816f4a2f2e3d5b1fbc200229a18497ed65e
Cara Lisy <lisyce@gmail.com>
2022-09-11 21:35:41 -0700

    initial ThreadCard component

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,9 +1,18 @@
 import Navbar from './components/Navbar';
+import ThreadCard from './components/ThreadCard'
 
 function App() {
+  const threadData = {
+    brand: 'DMC',
+    brand_number: 445,
+    name: 'blorple',
+    hex_value: 'dodgerblue'
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
+      <ThreadCard thread_data={threadData} skeins_owned='2'/>
     </div>
   );
 }
--- react_app/src/components/Navbar.js
+++ react_app/src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Navbar() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Floss Tracker</a>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <a className='navbar-brand' href='#'>Floss Tracker</a>
 
-        <div className="collapse navbar-collapse">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a className="nav-link" href="#">Collection</a>
+        <div className='collapse navbar-collapse'>
+            <ul className='navbar-nav'>
+                <li className='nav-item active'>
+                    <a className='nav-link' href='#'>Collection</a>
                 </li>
             </ul>
         </div>
