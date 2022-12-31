9fa18857474f52d035fb45c4f69bb7525c451c7a
lisyce <lisyce@gmail.com>
2022-12-31 15:26:10 -0800

    installing bootstrap icons to reduce load time

--- FlossTrackerApp/react_app/src/index.jsx
+++ FlossTrackerApp/react_app/src/index.jsx
@@ -1,5 +1,6 @@
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
